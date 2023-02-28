b6782f095593b87715e4a0adf1aff20df2877968
ivan <ivannrudoy@gmail.com>
2023-02-28 13:09:03 +0300

    feat: add import `List` impementation

--- src/pages/Repositories/Repositories.tsx
+++ src/pages/Repositories/Repositories.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import { HTMLAttributes, memo } from "react";
 
+import List from "./components/List";
+
 type RepositoriesProps = {} & HTMLAttributes<HTMLDivElement>;
 
-const Repositories: React.FC<RepositoriesProps> = () => <></>;
+const Repositories: React.FC<RepositoriesProps> = () => {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+};
 
 export type { RepositoriesProps };
 export default memo(Repositories);
---
+++ src/pages/Repositories/components/List/List.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { HTMLAttributes } from "react";
+
+type ListProps = {} & HTMLAttributes<HTMLDivElement>;
+
+const List: React.FC<ListProps> = () => <></>;
+
+export type { ListProps };
+export default List;
