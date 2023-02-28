2b95ee1e44a6c25aa456c35e79bc290b438f02bd
ivan <ivannrudoy@gmail.com>
2023-02-28 13:36:39 +0300

    feat: add store import

--- src/pages/Repositories/components/List/List.tsx
+++ src/pages/Repositories/components/List/List.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { HTMLAttributes } from "react";
 
+import { useLocalStore } from "@hooks/useLocalStore";
+
 type ListProps = {} & HTMLAttributes<HTMLDivElement>;
 
-const List: React.FC<ListProps> = () => <></>;
+const List: React.FC<ListProps> = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const repositoriesStore = useLocalStore(() => null);
+
+  return <></>;
+};
 
 export type { ListProps };
 export default List;
