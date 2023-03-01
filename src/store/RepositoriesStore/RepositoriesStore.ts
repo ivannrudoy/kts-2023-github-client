8a38f79bff22562dbb3718fe7e14a8590ceeb97c
ivan <ivannrudoy@gmail.com>
2023-03-01 15:36:23 +0300

    feat: add `RepositoriesStore` prototype

--- src/store/RepositoriesStore/RepositoriesStore.ts
+++ src/store/RepositoriesStore/RepositoriesStore.ts
@@ -1,5 +1,32 @@
 import GithubStore from "@store/GithubStore";
+import { RepositoryApi, RepositoryModel } from "@store/models/Github";
+import { action, computed, makeObservable, observable } from "mobx";
 
-class RepositoriesStore extends GithubStore {}
+type PrivateFields = "_list";
+
+class RepositoriesStore extends GithubStore<RepositoryApi[], RepositoryModel> {
+  private _list: RepositoryModel[] = [];
+
+  constructor() {
+    super();
+    makeObservable<RepositoriesStore, PrivateFields>(this, {
+      _list: observable.ref,
+      list: computed,
+      setList: action,
+    });
+  }
+
+  get list(): RepositoryModel[] {
+    return this._list;
+  }
+
+  setList(val: RepositoryModel[]) {
+    this._list = val;
+  }
+
+  normalizeApiData(d: RepositoryApi[]): void {
+    throw new Error("Method not implemented.");
+  }
+}
 
 export default RepositoriesStore;
---
+++ src/store/models/Github/Repository.ts
@@ -0,0 +1,5 @@
+type RepositoryApi = {};
+
+type RepositoryModel = {};
+
+export { RepositoryApi, RepositoryModel };
