b50dd6ce3f72326b241c282d484b35067444497e
Ivan Rudoy <ivannrudoy.github@gmail.com>
2023-03-08 20:24:49 +0000

    feat: add `.repositories__name` implementation

--- src/pages/Repositories/components/Name/Name.tsx
+++ src/pages/Repositories/components/Name/Name.tsx
@@ -3,6 +3,8 @@ import React, { ChangeEvent, FC, HTMLAttributes } from "react";
 import Button from "@components/Button";
 import Input from "@components/Input";
 
+import styles from "./Name.module.scss";
+
 type NameProps = {
   handleNameInput: (ev: ChangeEvent<HTMLInputElement>) => void;
   handleNameClick: () => void;
@@ -11,10 +13,10 @@ type NameProps = {
 
 const Name: FC<NameProps> = ({ handleNameInput, handleNameClick, value }) => {
   return (
-    <>
+    <div className={styles["repositories__name"]}>
       <Input value={value} onChange={handleNameInput} />
       <Button onClick={handleNameClick} />
-    </>
+    </div>
   );
 };
 
