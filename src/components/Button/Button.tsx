d8bd1c9cb654c07789b5bb4dc85117db8f0f4ae9
ivan <ivannrudoy@gmail.com>
2023-03-08 18:13:45 +0300

    feat: add button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,11 @@
 import React, { ButtonHTMLAttributes, FC } from "react";
 
+import styles from "./Button.module.scss";
+
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: FC<ButtonProps> = ({ onClick }) => {
-  return <button onClick={onClick} />;
+  return <button className={styles["button"]} onClick={onClick} />;
 };
 
 export type { ButtonProps };
