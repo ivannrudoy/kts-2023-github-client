99ec7acd97b4d020dee5f5326a6c505fae0c3b59
ivan <ivannrudoy@gmail.com>
2023-03-08 17:51:15 +0300

    feat: add `input` styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,16 @@
 import React, { ChangeEvent, FC, InputHTMLAttributes } from "react";
 
+import styles from "./Input.module.scss";
+
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   onChange: (ev: ChangeEvent<HTMLInputElement>) => void;
   value: string;
 };
 
 export const Input: FC<InputProps> = ({ value, onChange }) => {
-  return <input value={value} onChange={onChange} />;
+  return (
+    <input className={styles["input"]} value={value} onChange={onChange} />
+  );
 };
 
 export default Input;
