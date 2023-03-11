2d5e00ed3faf213662022704f902f03ce9dc3638
ivan <ivannrudoy@gmail.com>
2023-03-11 14:09:43 +0300

    feat: add `back` implementation

--- src/pages/Repository/components/View/View.tsx
+++ src/pages/Repository/components/View/View.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { HTMLAttributes } from "react";
 
 import { RepositoryModel } from "@store/models/Github";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./View.module.scss";
 
 type ViewProps = {
   org: string;
@@ -11,27 +14,42 @@ type ViewProps = {
 } & HTMLAttributes<HTMLDivElement>;
 
 const View: React.FC<ViewProps> = ({ org, name, repository, readme }) => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <div>
-        <div>Back</div>
-        {org}/{name}
-      </div>
-      <div>
-        <a href={repository.org_url}>{org}</a>
-      </div>
-      {repository.topics.length > 0 && (
-        <div>
-          {repository.topics.map((topic) => (
-            <span key={topic}>{`${topic} `}</span>
-          ))}
-        </div>
-      )}
-      <div>{repository.stargazers_count}</div>
-      <div>{repository.watchers}</div>
-      <div>{repository.forks}</div>
-      <div dangerouslySetInnerHTML={{ __html: readme }} />
-    </div>
+    <>
+      <header>
+        <nav>
+          <div onClick={handleBack} className={styles.nav__back}></div>
+          <h1>
+            {org}/{name}
+          </h1>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <div>
+            <a href={repository.org_url}>{org}</a>
+          </div>
+          {repository.topics.length > 0 && (
+            <div>
+              {repository.topics.map((topic) => (
+                <span key={topic}>{`${topic} `}</span>
+              ))}
+            </div>
+          )}
+          <div>{repository.stargazers_count}</div>
+          <div>{repository.watchers}</div>
+          <div>{repository.forks}</div>
+        </section>
+        <section>
+          <article dangerouslySetInnerHTML={{ __html: readme }} />
+        </section>
+      </main>
+    </>
   );
 };
 
