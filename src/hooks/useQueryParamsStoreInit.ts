146fd2a3718e54b8a15593857e70837b26bf33a6
Ivan Rudoy <ivannrudoy@gmail.com>
2023-03-20 16:07:07 +0300

    feat: add default `search`

--- src/hooks/useQueryParamsStoreInit.ts
+++ src/hooks/useQueryParamsStoreInit.ts
@@ -1,12 +1,19 @@
 import { useEffect } from "react";
 
 import rootStore from "@store/RootStore";
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 export const useQueryParamsStoreInit = (): void => {
   const { search } = useLocation();
+  let [ _, setSearchParams ] = useSearchParams();
 
   useEffect(() => {
+    if (search === "") {
+      setSearchParams(new URLSearchParams({
+        "name": "ktsstudio",
+        "page": "1",   
+      }))
+    }
     rootStore.query.setSearch(search);
   }, [search]);
 };
