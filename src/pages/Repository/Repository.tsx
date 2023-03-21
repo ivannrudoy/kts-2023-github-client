640704af8d628ad73ffbebdeebb98db3584bd0ec
ivan <ivannrudoy@gmail.com>
2023-03-21 13:09:28 +0300

    refactor: remove `Repository` props

--- src/pages/Repository/Repository.tsx
+++ src/pages/Repository/Repository.tsx
@@ -1,19 +1,17 @@
 import * as React from "react";
-import { HTMLAttributes, useEffect } from "react";
+import { useEffect } from "react";
 
+import Loader from "@components/Loader";
 import { useLocalStore } from "@hooks/useLocalStore";
 import READMEStore from "@store/READMEStore";
 import RepositoryStore from "@store/RepositoryStore";
+import { ResponseState } from "@utils/ResponseState";
 import { observer } from "mobx-react-lite";
 import { useParams } from "react-router-dom";
 
 import View from "./components/View/View";
-import { ResponseState } from "@utils/ResponseState";
-import Loader from "@components/Loader";
 
-type RepositoryProps = {} & HTMLAttributes<HTMLDivElement>;
-
-const Repository: React.FC<RepositoryProps> = () => {
+const Repository: React.FC = () => {
   const { org = "", name = "" } = useParams();
   const repositoryStore = useLocalStore(() => new RepositoryStore());
   const readmeStore = useLocalStore(() => new READMEStore());
@@ -25,15 +23,17 @@ const Repository: React.FC<RepositoryProps> = () => {
 
   return (
     <>
-      { (repositoryStore.responseState === ResponseState.SUCCESS && readmeStore.responseState === ResponseState.SUCCESS)
-      ? <View
+      {repositoryStore.responseState === ResponseState.SUCCESS &&
+      readmeStore.responseState === ResponseState.SUCCESS ? (
+        <View
           org={org}
           name={name}
           repository={repositoryStore.data}
           readme={readmeStore.data}
         />
-      : <Loader />
-      }
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
