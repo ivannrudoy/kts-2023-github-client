712a0223180642b095c902bf236ce5bf0fc9bd85
Ivan Rudoy <ivannrudoy.github@gmail.com>
2023-03-21 22:20:48 +0000

    refactor: remove `Loader` div props

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,21 +1,26 @@
+import React, { FC } from "react";
+
 import classNames from "classnames";
-import React, { FC, HTMLAttributes, } from "react";
 
 import styles from "./Loader.module.scss";
 
 enum Size {
-    s = "s",
-    m = "m",
-    l = "l"
+  s = "s",
+  m = "m",
+  l = "l",
 }
 
-type LoaderProps = HTMLAttributes<HTMLDivElement> & {
-    size?: Size;
+type LoaderProps = {
+  size?: Size;
 };
 
 const Loader: FC<LoaderProps> = ({ size = Size.l }) => {
-    return <div className={classNames(styles.loader, styles[`loader_size_${size}`])}></div>
-}
+  return (
+    <div
+      className={classNames(styles.loader, styles[`loader_size_${size}`])}
+    ></div>
+  );
+};
 
 export default Loader;
 export type { LoaderProps };
