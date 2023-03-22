f42921632f2e7392275e029d57752a31072a86fb
Ivan Rudoy <ivannrudoy.github@gmail.com>
2023-03-22 22:06:50 +0000

    feat: add `Error` prototype

---
+++ src/pages/Repositories/components/Error/Error.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+type ErrorProps = {
+  message: string;
+};
+
+const Error: FC<ErrorProps> = () => {
+  return <></>;
+};
+
+export default Error;
+export type { ErrorProps };
